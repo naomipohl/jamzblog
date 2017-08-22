2ba545a7fe336b6873b368ff6dc44655a54a4ddb
Naomi Pohl <naomi.pohl@yahoo.com>
2017-08-22 15:37:52 -0400

    first commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,70 @@
+class PostsController < ApplicationController
+  before_action :find_post, only: [:edit, :update, :show]
+  before_action :index, only: [:destroy]
+
+  # Index action to render all posts
+  def index
+    @posts = Post.all
+  end
+
+  # New action for creating post
+  def new
+    @post = Post.new
+  end
+
+  # Create action saves the post into database
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      flash[:notice] = "Successfully created post!"
+      redirect_to post_path(@post)
+    else
+      flash[:alert] = "Error creating new post!"
+      render :new
+    end
+  end
+
+  # Edit action retrives the post and renders the edit page
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  # Update action updates the post with the new information
+  def update
+    if @post.update_attributes(post_params)
+      flash[:notice] = "Successfully updated post!"
+      redirect_to post_path(@post)
+    else
+      flash[:alert] = "Error updating post!"
+      render :edit
+    end
+  end
+
+  # The show action renders the individual post after retrieving the the id
+  def show
+  end
+
+  # The destroy action removes the post permanently from the database
+  def destroy
+    @post = Post.find(params[:id])
+    if @post.destroy
+      flash[:notice] = "Successfully deleted post!"
+    else
+      flash[:alert] = "Error deleting post!"
+    end
+    redirect_to root_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+
+  def find_post
+    @post = Post.find(params[:id])
+  end
+
+  #This authenticates admin whenever a post is to be created, updated or destroyed.
+  before_action :authenticate_admin!, except: [:index, :show]
+end
